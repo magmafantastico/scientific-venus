367acff1a68a47efbf774fea725df8e53a2b4844
dudusbarros <joseeduardobarros@gmail.com>
2015-05-07 02:23:50 -0300

    Init Venus Project

--- js/app.js
+++ js/app.js
@@ -1,10 +1,140 @@
-/**
- * Created by Eduardo Barros on 29/11/2014.
+/*!
+ * Magma SCI v0.5.0 (http://getvilla.org/)
+ * Copyright 2013-2015 Noibe Developers
+ * Licensed under MIT (https://github.com/noibe/villa/blob/master/LICENSE)
  */
 
-/**
- * Pierre Rossignol - http://www.pierrerossignol.fr -
- * @version v1.0
- */
+$('#btn').click(function() {
+	console.log(getResponse());
+});
 
-this.number=this._domElem.find(".content em"),this.title=this._domElem.find(".content h3"),this.sep=this._domElem.find(".content .sep"),this.textContainer=this._domElem.find(".content p"),this.illustration=this._domElem.find(".illustration"),this.controls=this._domElem.find(".controls"),this.counter=this.controls.find("strong"),this.createScene(),this.backgroundContainer=this._domElem.find(".background"),this.reduceLink=this._domElem.find(".reduce"),this.bindReduceLink()},UspSidebar.prototype.createScene=function(){this.scene=new Canva({id:"usp-sidebar-canvas",container:this._domElem,width:440,height:this.containerHeight}),this.sceneHover=new Canva({id:"usp-sidebar-hover-canvas",container:this._domElem,width:440,height:this.containerHeight})},UspSidebar.prototype.drawBackground=function(t){this.scene.clear(!0),t||(t=this.nextId),this.scene.context.globalCompositeOperation="source-over";var e=this.keypoints[t].images["blur-"+t],i=app._w,s=i/this.ratio;s<this.containerHeight&&(s=this.containerHeight,i=s*this.ratio);var o=(app._w-i)/2-app._w+440,a=(this.containerHeight-s)/2;this.scene.context.drawImage(e,o,a,i,s),this.scene.context.globalCompositeOperation="source-over",this.scene.context.fillStyle="rgba(255,255,255,0.6)",this.scene.context.beginPath(),this.scene.context.moveTo(0,0),this.scene.context.lineTo(440,0),this.scene.context.lineTo(440,this.containerHeight),this.scene.context.lineTo(0,this.containerHeight),this.scene.context.closePath(),this.scene.context.fill();var n=2*app._mainContainerHeight;this.scene.context.globalCompositeOperation="destination-out",this.scene.context.fillStyle="#08b9f3",this.scene.context.beginPath(),this.scene.context.moveTo(0,0),this.scene.context.lineTo(440-this.guideTop.x,0),this.scene.context.lineTo(440-this.guide.x,n),this.scene.context.lineTo(0,n),this.scene.context.lineTo(0,0),this.scene.context.closePath(),this.scene.context.fill(),this.isReduced&&(this.scene.context.globalCompositeOperation="destination-out",this.scene.context.fillStyle="#08b9f3",this.scene.context.beginPath(),this.scene.context.moveTo(0,0),this.scene.context.lineTo(390,0),this.scene.context.lineTo(390,n),this.scene.context.lineTo(0,n),this.scene.context.lineTo(0,0),this.scene.context.closePath(),this.scene.context.fill())},UspSidebar.prototype.bindReduceLink=function(){this.reduceLink.on("click",$.proxy(this.onReduceLinkClick,this))},UspSidebar.prototype.onReduceLinkClick=function(t){var e=this;t.preventDefault(),this.isReduced?(this.isReduced=!1,this.reduceLink.removeClass("on"),this._domElem.find(".reduced-controls").removeClass("on"),TweenMax.fromTo(this.guide,.7,{x:50},{x:440,ease:Quint.easeInOut,onUpdate:$.proxy(this.drawBackground,this),onComplete:function(){e.onSidebarFull()}}),TweenMax.fromTo(this.guideTop,.5,{x:50},{x:440,ease:Quint.easeInOut})):(this._domElem.removeClass("infos-on"),this.controls.removeClass("on"),this.reduceLink.removeClass("on"),TweenMax.fromTo(this.guide,.7,{x:440},{x:50,ease:Quint.easeInOut,onUpdate:$.proxy(this.drawBackground,this),onComplete:function(){e.onSidebarReduced()}}),TweenMax.fromTo(this.guideTop,.5,{x:440},{x:50,ease:Quint.easeInOut}))},UspSidebar.prototype.onSidebarReduced=function(){this._domElem.addClass("reduced"),this.keypointsSlug.length>1&&this._domElem.find
+var getResponse = function() {
+
+	var r = {
+		prontuario: {
+			_id: getTextField('prontuario__id'),
+			data: getTextField('prontuario_data')
+		},
+		paciente: {
+			nome: getTextField('paciente_nome'),
+			sexo: getActiveRadio('paciente_sexo'),
+			nascimento: getTextField('paciente_nascimento'),
+			religiao: getActiveRadio('paciente_religiao'),
+			religiaoNote: getTextField('paciente_religiao_note'),
+			etnia: getActiveRadio('paciente_etnia'),
+			etniaNote: getTextField('paciente_etnia_note'),
+			escolaridade: getActiveRadio('paciente_escolaridade'),
+			escolaridadeNote: getTextField('paciente_escolaridade_note'),
+			estadoCivil: getActiveRadio('paciente_estadoCivil'),
+			estadoCivilNote: getTextField('paciente_estadoCivil_note')
+		},
+		exameFisico: {
+			peso: getTextField('exameFisico_peso'),
+			altura: getTextField('exameFisico_altura'),
+			imc: getTextField('exameFisico_imc'),
+			pressaoArterial: getTextField('exameFisico_pressaoArterial'),
+			circunferenciaAbdominal: getTextField('exameFisico_circunferenciaAbdominal'),
+			circunferenciaCervical: getTextField('exameFisico_circunferenciaCervical')
+		},
+		antecedentes: {
+			situacao: getActiveCheckbox('antecedentes_situacao'),
+			tabagismo: getActiveRadio('antecedentes_tabagismo'),
+			hac: getActiveRadio('antecedentes_hac'),
+			hacType: getActiveRadio('antecedentes_hac_type'),
+			diabetes: getActiveRadio('antecedentes_diabetes'),
+			diabetesType: getActiveRadio('antecedentes_diabetes_type'),
+			hipotireoidismo: getActiveRadio('antecedentes_hipotireoidismo'),
+			hipotireoidismoTYpe: getActiveRadio('antecedentes_hipotireoidismo_type'),
+			note: getTextField('antecedentes_note')
+		},
+		uteroMioma: {
+			us: getTextField('uteroMioma_us'),
+			volumeInterino: getTextField('uteroMioma_volumeInterino'),
+			ovarioDireito: getTextField('uteroMioma_ovarioDireito'),
+			ovarioEsquerdo: getTextField('uteroMioma_ovarioEsquerdo'),
+			endometro: getTextField('uteroMioma_endometro'),
+			miomaQuantidade: getTextField('uteroMioma_miomaQuantidade'),
+			mioma_1_caracteristicas: getTextField('uteroMioma_mioma_1_caracteristicas'),
+			mioma_1_type: getActiveCheckbox('uteroMioma_mioma_1_type'),
+			mioma_2_caracteristicas: getTextField('uteroMioma_mioma_2_caracteristicas'),
+			mioma_2_type: getActiveCheckbox('uteroMioma_mioma_2_type'),
+			nd: getActiveCheckbox('uteroMioma_nd')
+		},
+		sangramento: {
+			pbacInicial: getTextField('sangramento_pbacInicial')
+		},
+		escalas: {
+			beckInicial: getTextField('escalas_beckInicial'),
+			vidaMioma: getTextField('escalas_vidaMioma')
+		},
+		exames: {
+			ht: getTextField('exames_ht'),
+			ferro: getTextField('exames_ferro'),
+			ferritina: getTextField('exames_ferritina'),
+			rdw: getTextField('exames_rdw'),
+			vcm: getTextField('exames_vcm'),
+			vitaminaD3: getTextField('exames_vitaminaD3'),
+			tsh: getTextField('exames_tsh'),
+			gj: getTextField('exames_gj'),
+			ct: getTextField('exames_ct'),
+			ldl: getTextField('exames_ldl'),
+			hdl: getTextField('exames_hdl'),
+			t4l: getTextField('exames_t4l')
+		},
+		conduta: {
+			conduta: getActiveRadio('conduta'),
+			cirurgia: getActiveRadio('conduta_cirurgia'),
+			hormonioTerapia: getActiveRadio('conduta_hormonioTerapia'),
+			hormonioTerapiaCiclico: getActiveCheckbox('conduta_hormonioterapia_ciclico'),
+			hormonioTerapiaContinuo: getActiveCheckbox('conduta_hormonioterapia_continuo'),
+			/*hormonioTerapiaName: getTextField('conduta_hormonioterapia_nome', 'name'),*/ // name
+			ainh: getTextField('conduta_ainh')
+		},
+		resultados: {
+			pbacFinal: getTextField('resultados_pbacFinal'),
+			beckFinal: getTextField('resultados_beckFinal'),
+			vidaMioma: getTextField('resultados_vidaMiomaFinal')
+		}
+
+	};
+
+	return r;
+
+};
+
+var getActiveCheckbox = function(a) {
+	var b, c;
+
+	b = $('input[name=' + a + ']:checked');
+	c = new Array();
+
+	for (var d = b.length; d--; )
+		c.push($(b[d]).val());
+
+	if (c.length > 0)
+		return c;
+
+	return false;
+};
+
+var getActiveRadio = function(a) {
+	var b, c;
+
+	b = document.getElementsByName(a);
+
+	for (c = b.length; c--; )
+		if (b[c].checked)
+			return b[c].value;
+
+	return false;
+};
+
+var getTextField = function(a) {
+	var b = document.getElementById(a).value;
+
+	if (b)
+		return b;
+
+	return false;
+};
+
