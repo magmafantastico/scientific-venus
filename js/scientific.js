359a3ae96b0ce76ac148489790efcbe20751e0a6
dudusbarros <joseeduardobarros@gmail.com>
2015-05-22 13:31:22 -0300

    Criaçao de metodos para construir campos de miomas

--- js/scientific.js
+++ js/scientific.js
@@ -180,6 +180,204 @@ newEmptyField = function (c) {
 };
 
 /**
+ * FUNÇOES DE MIOMA
+ */
+
+/**
+ * Cria elemento radio para elemento de Mioma
+ * @param a num
+ * @param b typeName
+ * @return radio element
+ */
+var newMiomaFieldRadio;
+newMiomaFieldRadio = function (a, b) {
+	var e = newNode('span', 'field');
+
+	var ea = newNode('input');
+	ea.type = 'radio';
+	ea.id = 'mioma_type_' + a + '_' + b;
+	ea.name = 'mioma_type_' + a;
+	ea.value = b;
+
+	var eb = newContentNode('label', b);
+	eb.htmlFor = ea.id;
+
+	e.appendChild(ea);
+	e.appendChild(eb);
+
+	return e;
+};
+
+/**
+ * Cria novo elemento de Mioma
+ * @param a num
+ * @return mioma element
+ */
+var newMiomaField;
+newMiomaField = function (a) {
+	var e = newNode('div', 'mioma');
+
+	var ea = newNode('div', 'mioma-length');
+
+	var eaa = newNode('span', 'field flex-column');
+
+	var eaaa = newNode('input');
+	eaaa.type = 'text';
+	eaaa.id = 'mioma_medida_' + a;
+	eaaa.name = 'mioma_medida_' + a;
+	eaaa.placeholder = a;
+
+	var eaab = newContentNode('label', 'medida');
+	eaab.htmlFor = eaaa.id;
+
+	eaa.appendChild(eaab);
+	eaa.appendChild(eaaa);
+
+	ea.appendChild(eaa);
+
+	var eb = newNode('div', 'mioma-type flex flex-wrap');
+
+	var eba = newNode('span', 'field');
+	var ebb = newNode('span', 'field');
+	var ebc = newNode('span', 'field');
+
+	// Para adicionar novo tipo, é só adicionar novo append de radio em um novo field
+	eba.appendChild(newMiomaFieldRadio(a, 'intramural'));
+	ebb.appendChild(newMiomaFieldRadio(a, 'submucoso'));
+	ebc.appendChild(newMiomaFieldRadio(a, 'subseroso'));
+
+	eb.appendChild(eba);
+	eb.appendChild(ebb);
+	eb.appendChild(ebc);
+
+	e.appendChild(ea);
+	e.appendChild(eb);
+
+	return e;
+};
+
+/**
+ * Obtem value da input 'medida' do elemento Mioma
+ * @param a mioma element
+ * @return value | bool
+ */
+var getMiomaLength;
+getMiomaLength = function(a) {
+	var b = a.getElementsByClassName('mioma-length')[0].getElementsByTagName('input')[0].value;
+	if (b)return b;
+	return false;
+};
+
+/**
+ * Obtem value da input 'tipo' do elemento Mioma
+ * @param a mioma element
+ * @return value | bool
+ */
+var getMiomaType;
+getMiomaType = function(a) {
+	var b = a.getElementsByClassName('mioma-type')[0].getElementsByTagName('input');
+
+	for (var i = b.length; i--; )
+		if (b[i].checked)
+			return b[i].value;
+
+	return false;
+};
+
+/**
+ * Obtem objeto de respostas do elemento Mioma
+ * @param a mioma element
+ * @return object
+ */
+var getMioma;
+getMioma = function(a) {
+	return {
+		length: getMiomaLength(a),
+		type: getMiomaType(a)
+	};
+};
+
+/**
+ * Obtem array de objetos de respostas de todos elementos Mioma
+ * @param a mioma element
+ * @return object
+ */
+var getMiomas;
+getMiomas = function() {
+	var a, b;
+
+	a = document.getElementsByClassName('mioma');
+	b = [];
+
+	for (var i = a.length; i--; ) {
+		b.push(getMioma(a[i]));
+	}
+
+	return b;
+};
+
+/**
+ * Cria novo elemento de Mioma
+ */
+var createMioma;
+createMioma = function() {
+	var a, n;
+
+	a = document.getElementById('dynf');
+	n = a.getElementsByClassName('mioma');
+
+	a.insertBefore(newMiomaField(n.length + 1), a.lastChild);
+};
+
+/**
+ * Remove último elemento de Mioma
+ */
+var removeMioma;
+removeMioma = function() {
+	var a, b, n;
+
+	a = document.getElementById('dynf');
+	b = a.getElementsByClassName('mioma');
+	n = b.length - 1;
+
+	if (n >= 0)
+		a.removeChild(b[n]);
+};
+
+/**
+ * Controla número de elementos de Mioma baseado em 'n'
+ * @param n num
+ */
+var controlMioma;
+controlMioma = function(n) {
+	var a, b, bn, i;
+
+	a = document.getElementById('dynf');
+	b = a.getElementsByClassName('mioma');
+	bn = b.length;
+
+	if (n > bn)
+		for (i = n - bn; i--; )
+			createMioma();
+	else if (n < bn)
+		for (i = bn - n; i--; )
+			removeMioma();
+};
+
+/**
+ * Trata valor de input que recebe número de elementos do Mioma
+ */
+var numberMioma;
+numberMioma = function() {
+	if (!isNaN(this.value)) {
+		var n = parseInt(this.value);
+
+		if (n >= 0)
+			controlMioma(n);
+	}
+};
+
+/**
  * EXTERNAL FUNCTIONS
  */
 
